bbe4b7009863f99305b6b49e3e467c483e72b41a
Leroborat <rein@leroborat.com>
2025-10-25 12:16:38 +0200

    For deployment 2

--- pawnshop/__manifest__.py
+++ pawnshop/__manifest__.py
@@ -18,9 +18,7 @@ branch-aware security, and Philippine-ready reports.
 
     # show your static/description/index.html and images
     'images': [
-        'static/description/icon.png',
         'static/description/odooimagesmall.gif',
-        'static/description/odooimagebig.gif',
     ],
 
     # Dependencies
